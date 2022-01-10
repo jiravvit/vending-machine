286deb0167fbb6dca411af8b1352eab9f538764d
jiravvit <jirabbit05@gmail.com>
2022-01-09 21:08:53 -0800

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # vending-machine
+BoB 10th 3차 공통 교육 과제 #1
---
+++ widget.cpp
@@ -0,0 +1,69 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    setControl();
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::changeMoney(int coin)
+{
+    money += coin;
+    ui->lcdNumber->display(money);
+    setControl();
+}
+
+void Widget::setControl()
+{
+    ui->pbCoffee->setEnabled(money >= 200);
+    ui->pbTea->setEnabled(money >= 150);
+    ui->pbMilk->setEnabled(money >= 100);
+}
+
+void Widget::on_pbCoin500_clicked()
+{
+    changeMoney(500);
+}
+
+void Widget::on_pbCoin100_clicked()
+{
+    changeMoney(100);
+}
+
+
+void Widget::on_pbCoin50_clicked()
+{
+    changeMoney(50);
+}
+
+
+void Widget::on_pbCoin10_clicked()
+{
+    changeMoney(10);
+}
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-200);
+}
+
+void Widget::on_pbTea_clicked()
+{
+    changeMoney(-150);
+}
+
+
+void Widget::on_pbMilk_clicked()
+{
+    changeMoney(-100);
+}
+
